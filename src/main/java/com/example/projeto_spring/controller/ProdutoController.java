1591506ed053f8b7a45c3637be36c79e6b79b7b1
erikaoliveiracorrea <erikaestudosaraujo@gmail.com>
2026-09-25 09:22:23 -0300

    Atualizando projeto

--- src/main/java/com/example/projeto_spring/controller/ProdutoController.java
+++ src/main/java/com/example/projeto_spring/controller/ProdutoController.java
@@ -2,7 +2,6 @@ package com.example.projeto_spring.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.projeto_spring.exceptions.RecursoNaoEncontrado;
 import com.example.projeto_spring.model.Produto;
 import com.example.projeto_spring.service.ProdutoService;
 
