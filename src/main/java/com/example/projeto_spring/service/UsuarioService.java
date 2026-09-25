067b656f6caadc0d9c1f2aa4aae5d67dbd1ec8d6
erikaoliveiracorrea <erikaestudosaraujo@gmail.com>
2026-09-25 09:15:44 -0300

    Criando UsuarioService

---
+++ src/main/java/com/example/projeto_spring/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package com.example.projeto_spring.service;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.projeto_spring.model.Usuario;
+import com.example.projeto_spring.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();// armazenar senhas criptografada
+    }
+
+    public Usuario registrarUsuario(String username, String password) {
+        String senhaCriptografada = passwordEncoder.encode(password);
+        Usuario usuario = new Usuario(username, senhaCriptografada);
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> buscarPorUsername(String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+}
